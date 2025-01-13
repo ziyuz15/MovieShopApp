b62ae44920a96cd1551bca8ea5302a70ea3933e0
ziyuz15 <ziyuz15@uci.edu>
2025-01-12 23:06:10 -0800

    data and er diagram

--- MovieShopApplication/ApplicationCore/obj/Debug/net8.0/ApplicationCore.AssemblyInfo.cs
+++ MovieShopApplication/ApplicationCore/obj/Debug/net8.0/ApplicationCore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApplicationCore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87c842115b119635b734c75666730cf19b819a3d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApplicationCore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApplicationCore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
